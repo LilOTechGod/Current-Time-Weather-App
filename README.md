af97d8e7685572243c862cf01efeffb9ca6773f7
unknown <perezoscar360@gmail.com>
2022-10-13 17:28:27 -0500

    pushing code to app to get it to function

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Current-Time-Weather-App
+# The Millennial Weather App
+
+## Description:
+
+As a new web developer, I always find myself in a mind state of innovation. Starting a challenge from start to finish is really the only way I learn and there's always something to make look better, work for efficient, and write less code. Web developers are constantly sharpening their tools and skills by studying, I found myself wanting more of myself and having the proper tools and skills therefore I made this project in order to demonstrate to myself that I can do this! You will be able to succeed! I went from no knowdledge to some knowledge and I won't stop here neither should you. The tool I was sharpening here are web API and functions. In order to understand these concepts more I made a functioning weather wepage based on what I've learned. I no longer have doubt about these concepts and are learnable if you simply put your mind into it, dive right in and dont't focus on anything until you feel like you are in a good spot. Perseverance is everything!
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Screenshot](#screenshot)
+- [Credits](#credits)
+- [License](#license)
+
+## Usage
+
+ To get the development environment running simply:
+ 1. Open the link in your default browser.
+ 2. Type in a city inside the search bar.
+ 3. See current weather and future weather displayed 
+
+## screenshot
+[Click here to open screenshot of The Millennial Weather App](./assets/_C__Users_Oscar%2520PH_Desktop_UTA-bootcamp_Challenge_Current-Time-Weather-App_index.html%20(1).png)
+
+## Credits
+
+Teacher assistant: Ian
+Teacher assistant: Diem
+Teacher assistant: Negin
+Zoom recording of bootcamp class on API: https://courses.bootcampspot.com/courses/2262/external_tools/249
+web API key: https://openweathermap.org/
+video tutorial: https://www.youtube.com/watch?v=6trGQWzg2AI
+
+
+## License
+
+MIt License 
---
+++ assets/script.js
@@ -0,0 +1,109 @@
+//setting and giving value to global variables.
+var apiKey = "d91f911bcf2c0f925fb6535547a5ddc9";
+var searchBtn = document.querySelector(".Btn");
+var searchCity = document.querySelector(".searchCity");
+var currentweather = document.querySelector(".weather-info");
+var futureWeather = document.querySelector(".future-forcast");
+var timeClass = document.querySelector(".time");
+var dateClass = document.querySelector(".date");
+
+var days = ["Sunday", "Monday", 'Tuesday', 'Wednesday', 'Thursday']
+
+// setinveral to set up month/date/time and display on main page
+setInterval(() => {
+    var time = new Date();
+    var month = time.getMonth();
+    var date = time.getDate();
+    var day = time.getDay ();
+    var hour = time.getHours();
+    var hoursFormat12 = hour >= 13 ? hour %12: hour
+    var minute = time.getMinutes();
+    var ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeClass.innerHTML = hoursFormat12 + ':' + minute+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+    dateClass.innerHTML = time.toDateString()
+
+}, 1000);
+
+//function for fetching city value and name, only displaying top 5 & 2 .then is always needed
+function getGeo() {
+    var searchCityValue = searchCity.value
+    console.log(searchCityValue);
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchCityValue + "&limit=5&appid=" + apiKey)
+        .then(function (res) {
+            return res.json()
+        }).then(function (data) {
+            console.log(data);
+            getWeather(data);
+        })
+}
+
+//function with value of data. fetched lon and lat, set units
+function getWeather(data) {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=" + apiKey + "&units=imperial")
+        .then(function (res) {
+            return res.json()
+        }).then(function (data) {
+            getCurrentWeather(data);
+            fiveDayForcast(data);
+        })
+}
+
+searchBtn.addEventListener("click", getGeo);
+
+//function to fetch data form api and displaying on main card.
+function getCurrentWeather(data) {
+    // console.log(data); variable for weather info from data
+    var temperature = data.list[0].main.temp;
+    var humidity = data.list[0].main.humidity;
+    var windSpeed = data.list[0].wind.speed;
+    var realFeel = data.list[0].main.feels_like;
+    // console.log(temperature); Create element for data to be displayed
+    var temp = document.createElement("p");
+    var hum = document.createElement("p");
+    var wind = document.createElement("p");
+    var feelsLike = document.createElement("p");
+    //Adding text to the variable-adding displayed name-var
+    temp.textContent = `temp: ${temperature}`;
+    hum.textContent = `Humidity: ${humidity}`;
+    wind.textContent = `Wind: ${windSpeed}`;
+    feelsLike.textContent = `Real Feel: ${realFeel}`;
+    //Appending element(p) to html when data is pulled
+    currentweather.appendChild(temp);
+    currentweather.appendChild(hum);
+    currentweather.appendChild(wind);
+    currentweather.appendChild(feelsLike);
+}
+
+//function for fetching and displaying weather forcast on cards.
+function fiveDayForcast(data) {
+    for(var i=0; i<data.list.length; i=i+8) {
+        console.log(data.list[i])
+        //Create Element for html and in order to display
+        var dayCard = document.createElement("div");
+        dayCard.setAttribute("class", "column border 10px solid p-3");
+        dayCard.setAttribute("id", "wide");
+        // Create var with a value from the list(data)
+        var date = data.list[i].dt_txt;
+        var temp = data.list[i].main.temp;
+        var wind = data.list[i].wind.speed;
+        var hum = data.list[i].main.humidity;
+        var infoArray = [temp, wind, hum];
+        dayCard.append(date);
+        for(var j=0; j<infoArray.length; j++) {
+            var dayCardDiv = document.createElement("p");
+            dayCardDiv.setAttribute("class", "futureWeather");
+            dayCard.textContent = `Date: ${date}`;
+            dayCardDiv.textContent = `Temp: ${temp}`;
+            dayCardDiv.textContent = `Wind: ${wind}`;
+            dayCardDiv.textContent = `Humidity: ${hum}`;
+            dayCardDiv.append(infoArray[j]);
+            dayCard.append(dayCardDiv)
+        }
+        //Add element with value of var.value of data as above
+        // dayCardDiv.append(wind);
+        // dayCardDiv.append(hum);
+        futureWeather.appendChild(dayCard);
+    }
+}
